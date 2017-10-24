5cff303d7252d66945f001978da436a9542586d8
IgorKozarchuk <igor.a.kozarchuk@gmail.com>
2017-10-24 22:46:09 +0300

    partners

--- app/js/common.js
+++ app/js/common.js
@@ -102,4 +102,28 @@ $(function() {
 		smartSpeed: 700,
 		autoHeight: true
 	});
+
+	// partners
+	$('.partners').owlCarousel({
+		loop: true,
+		margin: 40,
+		smartSpeed: 700,
+		nav: true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			480: {
+				items: 2
+			},
+			768: {
+				items: 3
+			},
+			1200: {
+				items: 4
+			}
+		}
+	});
 });
